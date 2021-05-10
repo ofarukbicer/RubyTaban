7667d812f6ae2fea0eae27ed366efc7aa152330e
Ömer Faruk Biçer <omerfarukbicer0446@gmail.com>
2021-05-11 01:19:02 +0300

    değişklik

--- lib/RubyTaban.rb
+++ lib/RubyTaban.rb
@@ -6,69 +6,69 @@ require 'json'
 using RubyFiglet
 
 class RubyTaban
-  attr_accessor :banner,:girinti
+  attr_accessor :banner,:tabs
 
-  def initialize(banner, girinti = 1)
+  def initialize(banner, tabs = 1)
     @banner  = banner
-    @girinti = girinti
-    logo_yazdir()
-    bilgi_yazdir()
+    @tabs = tabs
+    logo
+    information
   end
 
-  def logo_yazdir()
-    temizle()
-    puts ((" " * @girinti).to_s + @banner + "\n\n").art!.green
+  def logo
+    clear
+    puts ((" " * @tabs).to_s + @banner + "\n\n").art!.green
     puts "\n\n"
   end
 
-  def bilgi_yazdir()
+  def information
     time = Time.new
 
-    oturum = (ENV['USERNAME'].to_s + "@" + ENV["COMPUTERNAME"].to_s).cyan.center(44)
-    ip     = global_ip().yellow.center(44)
-    cihaz  = ENV["OS"].red.center(44)
-    zaman  = ((time.strftime("%d-%m-%Y")).to_s + " | " + (time.strftime("%k:%M")).to_s).center(30)
+    session       = (ENV['USERNAME'].to_s + "@" + ENV["COMPUTERNAME"].to_s).cyan.center(44)
+    ip           = global_ip.yellow.center(44)
+    pc           = ENV["OS"].red.center(44)
+    time_format  = ((time.strftime("%d-%m-%Y")).to_s + " | " + (time.strftime("%k:%M")).to_s).center(30)
     
-    bilgi  = "\t\t#{cihaz}\n\t\t#{zaman.yellow}\n\n\t\t#{oturum}\n\t\t#{ip}"
-    puts bilgi
+    information_text  = "\t\t#{pc}\n\t\t#{time_format.yellow}\n\n\t\t#{session}\n\t\t#{ip}"
+    puts information_text
     puts "\n"
   end
 
-  def log_salla(sol, orta, sag)
-    zaman = Time.new.strftime("%k:%M:%S")
-    if zaman[" "]
-      zaman[" "] = "0"
+  def log(left, center, right)
+    time = Time.new.strftime("%k:%M:%S")
+    if time[" "]
+      time[" "] = "0"
     end
-    if (!sol.is_a?(String) || !orta.is_a?(String) || !sag.is_a?(String))
-      bicimlendir = "[#{zaman}]".cyan + " Sadece String Değere izin verilir".yellow
-      puts bicimlendir
+    if (!left.is_a?(String) || !center.is_a?(String) || !right.is_a?(String))
+      format_text = "[#{time}]".cyan + " Sadece String Değere izin verilir".yellow
+      puts format_text
       return
     end
 
-    sol1  = "#{sol[0,12]}".red + ("~".cyan if sol.length > 13).to_s
-    orta1 = "#{orta[0,18]}".yellow + ("~".cyan if orta.length > 19).to_s
-    sag1  = "#{sag[0,13]}".magenta + ("~".cyan if sag.length  > 14).to_s
+    left1  = "#{left[0,12]}".red + ("~".cyan if left.length > 13).to_s
+    center1 = "#{center[0,18]}".yellow + ("~".cyan if center.length > 19).to_s
+    right1  = "#{right[0,13]}".magenta + ("~".cyan if right.length  > 14).to_s
     
     
-    bicimlendir = "[#{zaman}]".cyan + " #{sol1}" + (" " * (15 - sol[0,13].to_s.length)).to_s + "||".green + " #{orta1} " + (" " * (21 - orta[0,19].to_s.length)).to_s + "||".green + " #{sag1}"
-    puts bicimlendir
+    format_text = "[#{time}]".cyan + " #{left1}" + (" " * (15 - left[0,13].to_s.length)).to_s + "||".green + " #{center1} " + (" " * (21 - center[0,19].to_s.length)).to_s + "||".green + " #{right1}"
+    puts format_text
   end
 
-  def hata_salla(hata)
-    zaman = Time.new.strftime("%k:%M:%S")
-    if zaman[" "]
-      zaman[" "] = "0"
+  def error(exception)
+    time = Time.new.strftime("%k:%M:%S")
+    if time[" "]
+      time[" "] = "0"
     end
 
-    bicimlendir = "[#{zaman}]".cyan + " #{hata.class.name}".yellow + "  || ".magenta + "#{hata.message}"
-    puts bicimlendir
+    format_text = "[#{time}]".cyan + " #{exception.class.name}".yellow + "  || ".magenta + "#{exception.message}"
+    puts format_text
   end
 
-  def temizle()
+  def clear
     Gem.win_platform? ? (system "cls") : (system "clear")
   end
 
-  def global_ip()
+  def global_ip
     begin
       uri = URI("http://ip-api.com/json")
       res = Net::HTTP.get_response(uri)
